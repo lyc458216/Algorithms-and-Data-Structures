33fa0a28815eb9bc5123a66ae6c9fbbd96169554
LYC <liyichuan@baijia.com>
2023-05-09 14:58:54 +0800

    feat: 完全二叉树基本性质

--- src/dataStructure/binaryTree.js
+++ src/dataStructure/binaryTree.js
@@ -1,4 +1,10 @@
 // 完全二叉树的一些公式
+// 1、第n层的节点数最多为2^n个节点
+// 2、n层二叉树最多有2^0+……+2^n=2^(n+1)-1个节点
+// 3、第一个非叶子节点：length/2
+// 4、一个节点的孩子节点：2n、2n+1
+
+// 基本结构
 function Node(data,left,right){
     this.data = data;
     this.left = left;
